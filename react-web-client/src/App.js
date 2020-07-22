2407fbce5ad69936ff41b2c7c9d7a0fc71a20059
Krueger <Krueger@WIN-V1J0CEH93OB>
2020-07-22 17:45:13 +0700

    first project

---
+++ react-web-client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Test = () => {
+  return (
+      <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      222222222222
+      </header>
+      </div>
+);
+}
+
+export default Test;
