cda0c095a178f7aabb43fcec75d3fa20df96395b
butenko <butenko@BUTENKO>
2020-07-29 11:24:47 +0700

    Добавил обновление state при добавлении нового стиля художника

--- react-web-client/src/render.js
+++ react-web-client/src/render.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { setPainterStyle, getPainterStyle } from './redux/state';
+import { setPainterStyle, initializeData } from './redux/state';
 
 export let rerenderEntireTree = (state) => {
   ReactDOM.render(
@@ -14,7 +14,7 @@ export let rerenderEntireTree = (state) => {
   );
 };
 
-getPainterStyle();
+initializeData();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
