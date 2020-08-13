26eba924ee2e7a4b7fb50be692f00d39964b2b67
Krueger <Krueger@DESKTOP-SFI7C3S>
2020-08-13 19:55:19 +0700

    Добавлен шаблон для переводчика

---
+++ react-web-client/src/components/admin-panel/interpreter/InterpreterForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cs from './../../body/content/Content.module.css';
+import AddPainter from './add-painter/AddPainter';
+import DeletePainter from './delete-painter/DeletePainter';
+import UpdatePainter from './update-painter/UpdatePainter';
+
+
+const PainterForm = (props) => {
+    console.log('PAAAA', props);
+    
+    return (
+
+        <div className={cs.content}>
+            <AddPainter setPainter={props}/>
+            <DeletePainter deletePainter={props}/>
+            <UpdatePainter updatePainter={props}/>
+        </div>
+    );
+}
+
+export default PainterForm;
