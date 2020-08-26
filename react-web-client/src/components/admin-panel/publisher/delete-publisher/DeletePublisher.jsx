3cf12e5af594be8511d14986f3c3ba8949099cea
Krueger <Krueger@DESKTOP-SFI7C3S>
2020-08-26 20:26:22 +0700

    Изменил метод ввода издателя для удаления с

--- react-web-client/src/components/admin-panel/publisher/delete-publisher/DeletePublisher.jsx
+++ react-web-client/src/components/admin-panel/publisher/delete-publisher/DeletePublisher.jsx
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import "./DeletePublisher.css";
+import SelectOption from '../../../shared/select-component/SelectOption';
 
 class DeletePublisher extends Component {
     name = React.createRef();
+    publishers = [];
+    state = {
+        data: []
+    }
+    componentDidMount() {
+        this.getPublishers(20);
+    }
+
+    getPublishers = async (count) => {
+        const res = await this.props.deletePublisher.publisherMethods.getPublishersByCount(count);
+        res.data.previewPublishers.forEach(data => {
+            this.publishers.push(data);
+          });
+        this.setState({ data: res.data })
+    }
 
     deletePublisher = () => {
-        let currentName = this.name.current.value;
-        this.props.deletePublisher.publisherMethods.deletePublisher(currentName);
+        const id = this.publishers.find(x => x.name === this.name.current.value).id;
+        this.props.deletePublisher.publisherMethods.deletePublisher(id);
     }
+
     render() {
         return (
             <div className="add-form">
                 <h3>Delete publisher</h3>
-                <div>
-                    <input className="input" ref={this.name} placeholder="Название издателя" />
-                </div>
+                <select ref={this.name}>
+                    {this.publishers.map(data => { return <SelectOption key={data.id} name={data.name} /> })}
+                </select>
                 <div>
                     <button className="button" onClick={this.deletePublisher}>delete</button>
                 </div>
