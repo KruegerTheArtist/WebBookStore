675a2360b2fdaae2c5116b63fe50d266887b927b
Krueger <Krueger@DESKTOP-SFI7C3S>
2020-08-26 19:23:22 +0700

    Переделал на классовый компонент

--- react-web-client/src/components/admin-panel/painter-style/delete-painter-style/DeletePainterStyle.jsx
+++ react-web-client/src/components/admin-panel/painter-style/delete-painter-style/DeletePainterStyle.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./DeletePainterStyle.css";
 
 let Child = (props) => {
@@ -8,80 +8,47 @@ let Child = (props) => {
   );
 };
 
-const DeletePainterStyle = (props) => {
-  let name1 = React.createRef();
-  let sel = React.createRef();
+class DeletePainterStyle extends Component {
+  name = React.createRef();
+  painterStyles = []
 
-  let deletePainter = () => {
-    let painterStyle = props.painterStyles.painterStyles.find(ps => ps.name === name1.current.value);
-    props.deletePainterStyle.painterStyleMethods.deletePainterStyle(painterStyle.id);
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    this.getPainterStyles();
   }
 
-  let clear = () => {
-    name1.current.value = '';
+  getPainterStyles = async () => {
+    const res = await this.props.deletePainterStyle.painterStyleMethods.getPainterStyle();
+    res.data.forEach(data => {
+      this.painterStyles.push(data);
+    });
+    this.setState({ data: this.painterStyles })
   }
-  
-  const allPainterStyles = props.deletePainterStyle.state.content.painterStyles;
-  console.log('PRRRRRRRRRRRRRRRR', allPainterStyles);
-  let newArray = [{ name: 'stringstring', id: '1' }, { name: 'stringstring2', id: '2' }];
-  newArray = allPainterStyles.map((ps) => {
-    return <Child key={ps.id} name={ps.name} />;
-  });
-  return (
-    <div className="add-form">
-      <h3>Delete painter style</h3>
-      <div>
-        <input className="input" ref={name1} placeholder="Название жанра" />
-      </div>
-      <select ref={sel}>
-        {newArray}
-      </select>
-      <div>
-        <button className="button" onClick={deletePainter}>delete</button>
-        <button className="button" onClick={clear}>Clear</button>
-      </div>
 
-    </div >
-  );
+  deletePainter = () => {
+    const id = this.painterStyles.find(x => x.name === this.name.current.value).id;
+    this.props.deletePainterStyle.painterStyleMethods.deletePainterStyle(id);
+  }
+
+  render() {
+    return (
+      < div className="add-form" >
+        <h3>Delete painter style</h3>
+        <select className="select" ref={this.name}>
+          {this.painterStyles.map((ps) => { return <Child key={ps.id} name={ps.name} /> })}
+        </select>
+        <div className="action-buttons">
+          <button className="button" onClick={this.deletePainter}>delete</button>
+        </div>
+
+      </div >
+    )
+
+  }
+
 }
 
 export default DeletePainterStyle;
-
-
-
-// const Countries = [
-//     {countryName: 'Bob', countryAbv: 'Smith'},
-//     {countryName: 'Sam', countryAbv: 'Spade'},
-//     {countryName: 'Carol', countryAbv: 'Alexander'},
-//     {countryName: 'Sarah', countryAbv: 'Jones'}
-//   ]
-
-//   // Our child component that will display our names:
-
-
-
-//   const Parent = React.createClass({
-//     getInitialState: function() {
-//       return {countries: Countries};
-//     },
-//     render: function() {
-//       const countries = this.state.countries.map((country, i) => {
-//         return (
-//           <Child
-//             key={i}
-//             countryName={country.countryName}
-//             countryAbv={country.countryAbv}
-//           />
-//         );
-//       });
-//      return (
-//       <div className="country-select">
-//           <select>
-//          {countries}
-//          </select>  
-//       </div>
-
-
-//      );
-//     }
-//   });
