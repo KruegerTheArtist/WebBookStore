396dbcdf0d693d802f682026d8dfa9884f59e2ac
Krueger <Krueger@DESKTOP-SFI7C3S>
2020-08-26 21:13:48 +0700

    Переделал на класс

--- react-web-client/src/components/admin-panel/painter/update-painter/UpdatePainter.jsx
+++ react-web-client/src/components/admin-panel/painter/update-painter/UpdatePainter.jsx
@@ -1,53 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./../../AdminPanel.module.css";
 
-const UpdatePainter = (props) => {
-    let oldName = React.createRef();
-    let name = React.createRef();
-    let age = React.createRef();
-    let description = React.createRef();
-    let style = React.createRef();
+class UpdatePainter extends Component {
+    oldName = React.createRef();
+    name = React.createRef();
+    age = React.createRef();
+    description = React.createRef();
+    style = React.createRef();
 
-    let add = () => {
-        let currentoldName = oldName.current.value;
-        let currentName = name.current.value;
-        let currentAge = age.current.value;
-        let currentDescription = description.current.value;
-        let currentStyle = style.current.value;
-
-        props.updatePainter.painterMethods.updatePainter(currentoldName, currentName, currentAge, currentDescription, currentStyle );
+    name = React.createRef();
+    painters = [];
+    state = {
+        data: []
+    }
+    componentDidMount() {
+        this.getPainters(20);
     }
 
-    let clear = () => {
-        oldName.current.value = '';
-        name.current.value = '';
-        age.current.value = '';
-        description.current.value = '';
-        style.current.value = '';
+    getPainters = async (count) => {
+        const res = await this.props.updatePainter.painterMethods.getPainterByCount(count);
+        res.data.previewPainters.forEach(data => {
+            this.painters.push(data);
+        });
+        this.setState({ data: res.data })
     }
-    return (
-        <div className="add-form">
-            <h3>Update painter</h3>
-            <div>
-                <input className="input" ref={oldName} placeholder="Старое художника" />
-                <input className="input" ref={name} placeholder="Имя художника" />
 
-            </div>
-            <div>
-                <input className="input" ref={age} placeholder="Лет художнику" />
-            </div>
-            <div>
-                <input className="input" ref={description} placeholder="Описание" />
-            </div>
-            <div>
-                <input className="input" ref={style} placeholder="Стиль" />
-            </div>
-            <div>
-                <button className="button" onClick={add}>Add</button>
-                <button className="button" onClick={clear}>Clear</button>
-            </div>
-        </div >
-    );
+    add = () => {
+        let currentoldName = this.oldName.current.value;
+        let currentName = this.name.current.value;
+        let currentAge = this.age.current.value;
+        let currentDescription = this.description.current.value;
+        let currentStyle = this.style.current.value;
+
+        this.props.updatePainter.painterMethods.updatePainter(currentoldName, currentName, currentAge, currentDescription, currentStyle);
+    }
+
+    clear = () => {
+        this.oldName.current.value = '';
+        this.name.current.value = '';
+        this.age.current.value = '';
+        this.description.current.value = '';
+        this.style.current.value = '';
+    }
+
+    render() {
+        return (
+            <div className="add-form">
+                <h3>Update painter</h3>
+                <div>
+                    <input className="input" ref={this.oldName} placeholder="Старое художника" />
+                    <input className="input" ref={this.name} placeholder="Имя художника" />
+
+                </div>
+                <div>
+                    <input className="input" ref={this.age} placeholder="Лет художнику" />
+                </div>
+                <div>
+                    <input className="input" ref={this.description} placeholder="Описание" />
+                </div>
+                <div>
+                    <input className="input" ref={this.style} placeholder="Стиль" />
+                </div>
+                <div>
+                    <button className="button" onClick={this.add}>Add</button>
+                    <button className="button" onClick={this.clear}>Clear</button>
+                </div>
+            </div >
+        );
+    }
+
 }
 
 export default UpdatePainter;
