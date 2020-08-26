9b4234f163f94ab68754f8910b1543d660aefea6
Krueger <Krueger@DESKTOP-SFI7C3S>
2020-08-26 21:00:28 +0700

    Переделал на класс

--- react-web-client/src/components/admin-panel/painter/delete-painter/DeletePainter.jsx
+++ react-web-client/src/components/admin-panel/painter/delete-painter/DeletePainter.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import "./../../AdminPanel.module.css";
+import { Component } from 'react';
 
-const DeletePainter = (props) => {
+class DeletePainter extends Component {
 
-    let name = React.createRef();
+    name = React.createRef();
+    painters = [];
+    state = {
+        data: []
+    }
+    componentDidMount() {
+        this.getPublishers(20);
+    }
 
-    let deletePainter = () => {
+    getPublishers = async (count) => {
+        const res = await this.props.updatePublisher.publisherMethods.getPublishersByCount(count);
+        res.data.previewPublishers.forEach(data => {
+            this.publishers.push(data);
+        });
+        this.setState({ data: res.data })
+    }
+
+    deletePainter = () => {
         let currentName = name.current.value;
         props.deletePainter.painterMethods.deletePainter(currentName);
     }
-
-    let clear = () => {
-        name.current.value = '';
+    render() {
+        return (
+            <div className="add-form" >
+                <h3>Delete painter</h3>
+                <div>
+                    <input className="input" ref={name} placeholder="Имя художника" />
+                </div>
+                <div>
+                    <button className="button" onClick={deletePainter}>Delete</button>
+                </div>
+            </div >
+        );
     }
 
-    return (
-        <div className="add-form">
-            <h3>Delete painter</h3>
-            <div>
-                <input className="input" ref={name} placeholder="Имя художника" />
-            </div>
-            <div>
-                <button className="button" onClick={deletePainter}>Delete</button>
-                <button className="button" onClick={clear}>Clear</button>
-            </div>
-        </div >
-    );
 }
 
 export default DeletePainter;
