f3f8440c92deffaae7ca26aee841b69cdb757b3c
Krueger <Krueger@DESKTOP-SFI7C3S>
2020-08-26 20:05:49 +0700

    Переделал DeletePublisher на классовый компонент

--- react-web-client/src/components/admin-panel/publisher/delete-publisher/DeletePublisher.jsx
+++ react-web-client/src/components/admin-panel/publisher/delete-publisher/DeletePublisher.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./DeletePublisher.css";
 
-const DeletePublisher = (props) => {
-    let name = React.createRef();
-    
-    let deletePublisher = () => {
-        let currentName = name.current.value;
-        props.deletePublisher.publisherMethods.deletePublisher(currentName);
+class DeletePublisher extends Component {
+    name = React.createRef();
+
+    deletePublisher = () => {
+        let currentName = this.name.current.value;
+        this.props.deletePublisher.publisherMethods.deletePublisher(currentName);
+    }
+    render() {
+        return (
+            <div className="add-form">
+                <h3>Delete publisher</h3>
+                <div>
+                    <input className="input" ref={this.name} placeholder="Название издателя" />
+                </div>
+                <div>
+                    <button className="button" onClick={this.deletePublisher}>delete</button>
+                </div>
+
+            </div >
+        );
     }
 
-    let clear = () => {
-        name.current.value = '';
-    }
-
-    return (
-        <div className="add-form">
-            <h3>Delete publisher</h3>
-            <div>
-                <input className="input" ref={name} placeholder="Название издателя"/>
-            </div>
-            <div>
-                <button className="button" onClick={deletePublisher}>delete</button>
-                <button className="button" onClick={clear}>Clear</button>
-            </div>
-
-        </div >
-    );
 }
 
 export default DeletePublisher;
