876891bcbe17373b9a880ecc006595c00528ac74
butenko <butenko@BUTENKO>
2020-08-27 11:54:56 +0700

    Добавил чтение из settings.json для CoverType

--- react-web-client/src/redux/API/CoverTypes.jsx
+++ react-web-client/src/redux/API/CoverTypes.jsx
@@ -1,9 +1,12 @@
 import * as axios from "axios";
 import state, { initializeData } from "../state";
 import { newGuid } from "../shared/Helper";
+import config from '../../assets/settings.json';
+
+const controllerName = 'CoverType';
 
 export let getCoverTypes = () => {
-    axios.get('https://localhost:44394/api/CoverType/GetCoverTypes').then(response => {
+    axios.get(getUrl() + '/GetCoverTypes').then(response => {
         console.log('get cover type', response);
         state.contentPage.coverTypes = response.data;
     })
@@ -15,7 +18,7 @@ export let addCoverType = (name) => {
         id: newGuid(),
         name
     }
-    axios.post('https://localhost:44394/api/CoverType', сoverType).then(response => {
+    axios.post(getUrl(), сoverType).then(response => {
         console.log('add cover type', response);
     })
 
@@ -28,7 +31,7 @@ export let updateCoverType = (oldName, name) => {
         id: coverTypeId,
         name
     }
-    axios.put('https://localhost:44394/api/CoverType/', coverType).then(response => {
+    axios.put(getUrl(), coverType).then(response => {
         console.log('update cover type', response);
     })
 
@@ -38,8 +41,12 @@ export let updateCoverType = (oldName, name) => {
 export let deleteCoverType = (name) => {
     let coverTypeId = state.contentPage.coverTypes.find(ps => ps.name === name).id
 
-    axios.delete('https://localhost:44394/api/CoverType/' + coverTypeId).then(response => {
+    axios.delete(getUrl() + '/' + coverTypeId).then(response => {
         console.log('delete cover type', response);
     })
     initializeData();
+}
+
+let getUrl = () => {
+    return config.apiUrl + controllerName;
 }
